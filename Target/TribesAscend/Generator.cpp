fce9917f694b775ab8d80e032412c62d4ea319b7
KN4CK3R <admin@oldschoolhack.me>
2017-11-08 13:58:45 +0100

    Removed redundant specifier.

--- Target/TribesAscend/Generator.cpp
+++ Target/TribesAscend/Generator.cpp
@@ -6,7 +6,7 @@ class Generator : public IGenerator
 {
 public:
 
-	virtual bool Initialize(void* module) override
+	bool Initialize(void* module) override
 	{
 		predefinedStaticMembers["Class Core.Object"] = {
 			{ "TArray<UObject*>*", "GObjects" }
@@ -155,30 +155,32 @@ public:
 		return true;
 	}
 
-	virtual std::string GetGameName() const override
+	std::string GetGameName() const override
 	{
 		return "Tribes: Ascend";
 	}
-	virtual std::string GetGameNameShort() const override
+
+	std::string GetGameNameShort() const override
 	{
 		return "TA";
 	}
-	virtual std::string GetGameVersion() const override
+
+	std::string GetGameVersion() const override
 	{
 		return "1.3.2878.0";
 	}
 
-	virtual std::string GetNamespaceName() const override
+	std::string GetNamespaceName() const override
 	{
 		return "Classes";
 	}
 
-	virtual std::vector<std::string> GetIncludes() const override
+	std::vector<std::string> GetIncludes() const override
 	{
 		return { };
 	}
 
-	virtual std::string GetBasicDeclarations() const override
+	std::string GetBasicDeclarations() const override
 	{
 		return R"(template<typename Fn>
 inline Fn GetVFunction(const void *instance, std::size_t index)
@@ -481,7 +483,7 @@ struct FScriptDelegate
 };)";
 	}
 
-	virtual std::string GetBasicDefinitions() const override
+	std::string GetBasicDefinitions() const override
 	{
 		return R"(TArray<FNameEntry*>* FName::GNames = nullptr;
 TArray<UObject*>* UObject::GObjects = nullptr;)";
